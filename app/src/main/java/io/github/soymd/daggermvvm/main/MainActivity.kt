18882f13f2ed88b2d8244a3d894556e3280fdbd5
soymd <pacificstate33@gmail.com>
2021-06-06 10:14:17 +0900

    ViewModelFactoryを使用したViewModelの生成に戻す
    Took 19 minutes

--- app/src/main/java/io/github/soymd/daggermvvm/main/MainActivity.kt
+++ app/src/main/java/io/github/soymd/daggermvvm/main/MainActivity.kt
@@ -7,15 +7,26 @@ import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.soymd.daggermvvm.R
 import io.github.soymd.daggermvvm.databinding.ActivityMainBinding
+import io.github.soymd.daggermvvm.di.AppViewModelProviders
+import io.github.soymd.daggermvvm.di.ViewModelFactory
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel:MainViewModel by viewModels()
+    // todo:この方法でviewModelを生成してtestからinjectする方法が不明
+    //  private val viewModel:MainViewModel by viewModels()
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private var viewModelProviders = AppViewModelProviders()
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = viewModelProviders.of(this,viewModelFactory).get(MainViewModel::class.java)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.apply {
